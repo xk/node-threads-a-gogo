38c8f9d434f2738620ef9ec5a5cdbfaa3bb04a1e
Jorge Chamorro Bieling / @jorgechamorro <jorge@jorgechamorro.com>
2016-12-11 19:51:14 +0100

    tidy up queues agogo

--- src/queues_a_gogo.cc
+++ src/queues_a_gogo.cc
@@ -1,12 +1,6 @@
 //2011-11 Proyectos Equis Ka, s.l., jorge@jorgechamorro.com
 //queues_a_gogo.cc
 
-#include <unistd.h>
-#include <string.h>
-#include <stdio.h>
-#include <pthread.h>
-#include <stdlib.h>
-
 enum types {
   kItemTypeNONE,
   kItemTypeNumber,
@@ -22,21 +16,17 @@ struct typeQueueItem {
     double asNumber;
   };
 };
-typedef struct typeQueueItem typeQueueItem;
 
-typedef struct {
-  typeQueueItem* first;
-  typeQueueItem* last;
-  pthread_mutex_t queueLock;
+struct typeQueue {
   long int id;
+  typeQueueItem* last;
+  typeQueueItem* first;
   volatile long int length;
-} typeQueue;
+  pthread_mutex_t queueLock;
+};
 
-static typeQueue* queuesPool= NULL;
+//static typeQueue* queuesPool= NULL;
 static typeQueue* freeItemsQueue= NULL;
-
-
-
 
 static void queue_push (typeQueueItem* qitem, typeQueue* queue) {
   qitem->next= NULL;
@@ -48,91 +38,63 @@ static void queue_push (typeQueueItem* qitem, typeQueue* queue) {
   else {
     queue->first= qitem;
   }
+  queue->length+= 1;
   queue->last= qitem;
-  queue->length++;
   pthread_mutex_unlock(&queue->queueLock);
 }
 
-
-
-
 static typeQueueItem* queue_pull (typeQueue* queue) {
-  typeQueueItem* qitem;
+
+  typeQueueItem* qitem= NULL;
   
-  pthread_mutex_lock(&queue->queueLock);
-  if ((qitem= queue->first)) {
-    if (queue->last == qitem) {
-      queue->first= queue->last= NULL;
+  if (queue->first) {
+    pthread_mutex_lock(&queue->queueLock);
+    qitem= queue->first;
+    if (qitem) {
+      if (queue->last == qitem)
+        queue->first= queue->last= NULL;
+      else
+        queue->first= qitem->next;
+      queue->length-= 1;
+      qitem->next= NULL;
     }
-    else {
-      queue->first= qitem->next;
-    }
-    queue->length--;
-    qitem->next= NULL;
+    pthread_mutex_unlock(&queue->queueLock);
   }
-  pthread_mutex_unlock(&queue->queueLock);
   
   return qitem;
 }
-
-
-
 
 static typeQueueItem* nuItem (int itemType, void* item) {
   
   typeQueueItem* qitem= queue_pull(freeItemsQueue);
-  if (!qitem) {
-    qitem= (typeQueueItem*) calloc(1, sizeof(typeQueueItem));
-  }
-  
+  if (!qitem) qitem= (typeQueueItem*) malloc(sizeof(typeQueueItem));
   qitem->next= NULL;
   qitem->itemType= itemType;
-  if (itemType == kItemTypeNumber) {
+  if (itemType == kItemTypeNumber)
     qitem->asNumber= *((double*) item);
-  }
-  else if (itemType == kItemTypePointer) {
+  else if (itemType == kItemTypePointer)
     qitem->asPtr= item;
-  }
   
   return qitem;
 }
 
-
-
-
 static void destroyItem (typeQueueItem* qitem) {
-  
-  if (freeItemsQueue) {
+  if (freeItemsQueue)
     queue_push(qitem, freeItemsQueue);
-  }
-  else {
+  else
     free(qitem);
-  }
 }
 
-
-
-
 static typeQueue* nuQueue (long int id) {
-  
-  typeQueue* queue= NULL;
-  typeQueueItem* qitem= NULL;
-  if (queuesPool && queuesPool->first) qitem= queue_pull(queuesPool);
-  if (qitem) {
-    queue= (typeQueue*) qitem->asPtr;
-    destroyItem(qitem);
-  }
-  else {
-    queue= (typeQueue*) calloc(1, sizeof(typeQueue));
-    pthread_mutex_init(&queue->queueLock, NULL);
-  }
-  
+
+  typeQueue* queue= (typeQueue*) malloc(sizeof(typeQueue));
   queue->id= id;
   queue->length= 0;
   queue->first= queue->last= NULL;
+  pthread_mutex_init(&queue->queueLock, NULL);
+  
   return queue;
 }
-
 
 /*
 
@@ -147,12 +109,7 @@ static void destroyQueue (typeQueue* queue) {
 
 */
 
-
 static void initQueues (void) {
   freeItemsQueue= nuQueue(-2);  //MUST be created before queuesPool
   //queuesPool= nuQueue(-1);
 }
-
-
-
-
